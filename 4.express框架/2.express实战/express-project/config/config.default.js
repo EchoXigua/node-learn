b051aa13d760861e60e8a626477d106b112e8f42
EchoXigua <643589668@qq.com>
2022-12-20 19:11:05 +0800

    服务器处理

--- 4.express框架/2.express实战/express-project/config/config.default.js
+++ 4.express框架/2.express实战/express-project/config/config.default.js
@@ -8,9 +8,9 @@ module.exports.uuid = 'd01d07cb-609b-4a2a-a291-2d8c7782cd35'
 module.exports.mongoPath = 'mongodb://localhost:27017/express-video'
 
 module.exports.redisClient = {
-  host: '124.223.180.25',
+  host: '127.0.0.1',
   port: 6379,
   options: {
-    password: 'xigua150522'
+    password: 'root'
   }
 }
